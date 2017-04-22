cc40bdda3f219f7605f4bd4a99fd4b47aa8784dd
Dmitry Kulagin <kulaginds@gmail.com>
2017-04-22 20:47:54 +0300

    cleaned templates cache

--- engine/templates_c/%%0E^0E4^0E407559%%footer.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.30, created on 2017-04-22 20:41:44
-         compiled from footer.tpl */ ?>
-		</div>
-		<footer>
-			<p>Code licensed <a href="/LICENSE">GNU GPLv3</a>. <span class="right">Автор: <a href="https://scorpio3.ru">Дмитрий Кулагин</a>.</span></p>
-		</footer>
-	</div>
-</body>
-</html>
