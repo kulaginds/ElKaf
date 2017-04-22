d1caae7f4732aca55b11985513b9c690d1b55d94
Dmitry Kulagin <kulaginds@gmail.com>
2017-04-22 20:50:13 +0300

    fixed: no templates cache dir

--- engine/header.php
+++ engine/header.php
@@ -3,6 +3,10 @@
 require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/modules/smarty/Smarty.class.php');
 
+if (!is_dir(__DIR__.'/templates_c/')) {
+	mkdir(__DIR__.'/templates_c/');
+}
+
 $smarty = new Smarty();
 $smarty->template_dir = __DIR__.'/templates/';
 $smarty->compile_dir = __DIR__.'/templates_c/';
