f9505cc09861429599bebcbfcdc086462d335a87
Dmitry Kulagin <kulaginds@gmail.com>
2017-04-23 01:32:21 +0300

    added comments

--- engine/footer.php
+++ engine/footer.php
@@ -1,9 +1,13 @@
 <?php
 
+// собираю текст из буфера
 $content = ob_get_contents();
+// чищу буфер
 ob_end_clean();
 
+// подключаю текст контента в шаблон
 $smarty->assign('content', $content);
+// рендерю и отображаю страницу
 $smarty->display('basic.tpl');
 
 ?>
--- engine/header.php
+++ engine/header.php
@@ -3,15 +3,20 @@
 require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/modules/smarty/Smarty.class.php');
 
+// создам папку для скомпиленных шаблонов, если нужно
 if (!is_dir(__DIR__.'/templates_c/')) {
 	mkdir(__DIR__.'/templates_c/');
 }
 
+// инициализация шаблонизатора
 $smarty = new Smarty();
 $smarty->template_dir = __DIR__.'/templates/';
 $smarty->compile_dir = __DIR__.'/templates_c/';
+
+// подключаю параметры шаблона из конфига
 $smarty->assign('config', $config['template']);
 
+// включаю буферизацию вывода
 ob_start();
 
 ?>
