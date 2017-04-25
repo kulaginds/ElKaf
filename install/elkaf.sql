de4b53dbcffde0f9237959af4697137982e30f40
Dmitry Kulagin <kulaginds@gmail.com>
2017-04-25 20:00:35 +0300

    added install mechanism

---
+++ install/elkaf.sql
@@ -0,0 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Апр 24 2017 г., 11:41
+-- Версия сервера: 5.7.17-0ubuntu0.16.04.2
+-- Версия PHP: 7.0.15-0ubuntu0.16.04.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `elkaf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `couple`
+--
+
+CREATE TABLE `couple` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `discipline_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `schedule_id` int(10) UNSIGNED NOT NULL,
+  `auditory` varchar(10) NOT NULL,
+  `couple_number` tinyint(3) UNSIGNED NOT NULL,
+  `weekday` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `discipline`
+--
+
+CREATE TABLE `discipline` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `literature` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `document`
+--
+
+CREATE TABLE `document` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `discipline_id` int(10) UNSIGNED NOT NULL,
+  `file_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `group`
+--
+
+CREATE TABLE `group` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `group_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password_hash` char(32) DEFAULT NULL,
+  `type` enum('administrator','teacher','student') NOT NULL DEFAULT 'student',
+  `description` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_discipline`
+--
+
+CREATE TABLE `user_discipline` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `discipline_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_group`
+--
+
+CREATE TABLE `user_group` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `group_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `couple`
+--
+ALTER TABLE `couple`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auditory` (`auditory`,`couple_number`,`weekday`),
+  ADD KEY `discipline_id` (`discipline_id`),
+  ADD KEY `schedule_id` (`schedule_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `discipline`
+--
+ALTER TABLE `discipline`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `document`
+--
+ALTER TABLE `document`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `discipline_id` (`discipline_id`);
+
+--
+-- Индексы таблицы `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_id` (`group_id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user_discipline`
+--
+ALTER TABLE `user_discipline`
+  ADD PRIMARY KEY (`user_id`,`discipline_id`),
+  ADD KEY `discipline_id` (`discipline_id`);
+
+--
+-- Индексы таблицы `user_group`
+--
+ALTER TABLE `user_group`
+  ADD PRIMARY KEY (`user_id`,`group_id`),
+  ADD KEY `group_id` (`group_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `couple`
+--
+ALTER TABLE `couple`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `discipline`
+--
+ALTER TABLE `discipline`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `document`
+--
+ALTER TABLE `document`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `group`
+--
+ALTER TABLE `group`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `couple`
+--
+ALTER TABLE `couple`
+  ADD CONSTRAINT `couple_ibfk_1` FOREIGN KEY (`discipline_id`) REFERENCES `discipline` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `couple_ibfk_2` FOREIGN KEY (`schedule_id`) REFERENCES `schedule` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `couple_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `document`
+--
+ALTER TABLE `document`
+  ADD CONSTRAINT `document_ibfk_1` FOREIGN KEY (`discipline_id`) REFERENCES `discipline` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `user_discipline`
+--
+ALTER TABLE `user_discipline`
+  ADD CONSTRAINT `user_discipline_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_discipline_ibfk_2` FOREIGN KEY (`discipline_id`) REFERENCES `discipline` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `user_group`
+--
+ALTER TABLE `user_group`
+  ADD CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ install/index.php
@@ -0,0 +1,88 @@
+<?php
+
+include(__DIR__.'/../engine/header.php');
+
+function step1() {
+	global $db;
+
+	$sql = __DIR__.'/elkaf.sql';
+
+	echo '<h1>Шаг 1: установка структуры базы данных</h1>';
+
+	if (!is_file($sql)) {
+		echo '<p class="error">Отсутствует необходимый для установки файл <i>/install/elkaf.sql</i>.</p>';
+		return;
+	}
+
+	$sql = file_get_contents($sql);
+
+	if ($db->multi_query($sql)) {
+		echo '<p>Создание структуры завершено. Перейти ко <a href="/install/index.php?step=2">второму шагу</a>.</p>';
+	} else {
+		echo '<p class="error">Не удалось создать структуру БД.</p>';
+	}
+}
+
+function step2() {
+	echo '<h1>Шаг 2: добавление администратора</h1>';
+	echo '<form action="/install/index.php?step=3" method="POST">';
+	echo '<label>ФИО администратора: <input type="text" name="name"></label><br>';
+	echo '<label>Пароль администратора: <input type="password" name="password"></label><br>';
+	echo '<ul class="menu mb">';
+	echo '<li><input type="submit" value="Далее"></li>';
+	echo '</ul>';
+	echo '</form>';
+}
+
+function step3() {
+	global $db, $config;
+
+	if (!array_key_exists('name', $_POST) || empty($_POST['name']) || !array_key_exists('password', $_POST) || empty($_POST['password'])) {
+		error_step();
+		return;
+	}
+
+	echo '<h1>Финиш</h1>';
+
+	$name          = trim(htmlspecialchars($_POST['name']));
+	$password      = trim(htmlspecialchars($_POST['password']));
+	$password_hash = md5($config['security']['auth_salt'].$password);
+
+	$stmt = $db->prepare('INSERT INTO user(name, password_hash, type) VALUES(?, ?, \'administrator\')');
+	$stmt->bind_param('ss', $name, $password_hash);
+
+	if ($stmt->execute()) {
+		echo '<p>Установка успешно завершена!</p>';
+		echo '<p><b>Пожалуйста</b>, удалите папку <i>/install/</i>.</p>';
+	} else {
+		echo '<p class="error">Не удалось добавить администратора.</p>';
+	}
+}
+
+function error_step() {
+	echo '<p class="error">Неизвестный шаг.</p>';
+}
+
+if (array_key_exists('step', $_GET) && !empty($_GET['step'])) {
+	switch ($_GET['step']) {
+		case '1':
+			step1();
+			break;
+		case '2':
+			step2();
+			break;
+		case '3':
+			step3();
+			break;
+		
+		default:
+			step1();
+			break;
+	}
+} else {
+	step1();
+}
+
+include(__DIR__.'/../engine/footer.php');
+
+?>
